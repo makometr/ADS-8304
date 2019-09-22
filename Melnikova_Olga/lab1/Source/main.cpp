3f149e162a4a105c6358f81dbd353130a0f38eb3
olga-alexandrovnaa <melnikova.oa00@gmail.com>
2019-09-22 20:20:50 +0300

    fixed

--- Melnikova_Olga/lab1/Source/main.cpp
+++ Melnikova_Olga/lab1/Source/main.cpp
@@ -3,75 +3,74 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <vector>
 
-#define SIZE 1000
 
-using namespace std;
-
-int GetRoot(int x, int* sets)  //для нахождения представителя, подняться вверх по родительским ссылкам до тех пор, пока не наткнемся на корень
+int GetRoot(int x, std::vector<int>* sets)  //для нахождения представителя, подняться вверх по родительским ссылкам до тех пор, пока не наткнемся на корень
 //для каждой вершины по пути от X к корню изменим предка на этого самого представителя
 {
-	return (sets[x] == x) ? x : sets[x] = GetRoot(sets[x], sets);
+	return ((*sets)[x] == x) ? x : (*sets)[x] = GetRoot((*sets)[x], sets);
 }
 
-void UnionBranch(int x, int y, int* sets, int* ranks)
+void UnionBranch(int x, int y, std::vector<int>* sets, std::vector<int>* ranks)
 {
     if ( (x = GetRoot(x, sets)) == (y = GetRoot(y, sets)) )
         return;
 
-    if ( ranks[x] < ranks[y] )  //подвешиваем более низкое дерево к более высокому
-        sets[x] = y;
+    if ( (*ranks)[x] < (*ranks)[y] )  //подвешиваем более низкое дерево к более высокому
+        (*sets)[x] = y;
     else
     {
-        sets[y] = x;
-        if ( ranks[x] == ranks[y] )
-            ranks[x]++;
+        (*sets)[y] = x;
+        if ( (*ranks)[x] == (*ranks)[y] )
+            (*ranks)[x]++;
     }
 }
 
 
-int GetManID(string name, string* people, int peopleCount)
+int GetManID(std::string name, std::vector<std::string>* people, int peopleCount)
 {
 	for (int i = 0; i < peopleCount; i++)
-		if (people[i] == name)
+		if ((*people)[i] == name)
 			return i;
 	//в цикле по people сравниваем name c people[i]. если равно, то возвращаем i
 }
 
-void ReadAndWritePeople(string* people, int* parents, int* children, int* peopleCount, int* relationsCount)
+void ReadAndWritePeople(std::vector<std::string>* people, std::vector<int>* parents, std::vector<int>* children, int* peopleCount, int* relationsCount)
 {
 	// в цикле считываем строки
-	string str;
-	ifstream file("relations.txt", ios::in);
+	std::string str;
+	std::ifstream file("relations.txt", std::ios::in);
 	while(!file.eof())
 	{
-		getline(file, str);
+		std::getline(file, str);
 		char* cstr = new char[str.length()+1];
 		strcpy(cstr, str.c_str());
 		char* name = new char[20];
-		name = strtok (cstr, " "); 
+		name = strtok (cstr, " ");
 
 		int flagChild = 0;
- 		string parentName;
+ 		std::string parentName;
  		int parentID;
 
 		// ищем имена в people[]
 		// если имени нет - добавить
 
-		while (name != NULL)                 
+		while (name != NULL)
 		{
 			int flag = 1;
 			for(int i = 0; i < *peopleCount; i++){
-				if(strcmp(name, people[i].c_str()) == 0) { flag = 0;	break; }
+				if(strcmp(name, (*people)[i].c_str()) == 0) { flag = 0;	break; }
 			}
 			if(flag){
-				people[*peopleCount] = name;
+				people->push_back(name);
+				//people[*peopleCount] = name;
 				(*peopleCount)++;
 			}
 
-			if(flagChild){      // если считали второе и последующие имена в строке, то добавляем отношение 
-				parents[*relationsCount] = parentID;   //(индекс первого имени в строке - индекс считанного имени)
-				children[*relationsCount] = GetManID(name, people, *peopleCount);
+			if(flagChild){      // если считали второе и последующие имена в строке, то добавляем отношение
+				parents->push_back(parentID);   //(индекс первого имени в строке - индекс считанного имени)
+				children->push_back(GetManID(name, people, *peopleCount));
 				(*relationsCount)++;
 			}
 			else
@@ -85,69 +84,71 @@ void ReadAndWritePeople(string* people, int* parents, int* children, int* people
 			flagChild = 1;
 		}
 	}
-	
-	cout << "\nСписок всех жителей:\n" << endl;
+
+	std::cout << "\nСписок всех жителей:" << std::endl;
 	for (int i = 0; i < *peopleCount; i++){
-        cout << i << " " << people[i] << "\n" << endl;
+        std::cout << i << " " << (*people)[i] << std::endl;
     }
-    
-    cout << "\nТаблица отношений в индексах жителей:\n" << endl;
+
+    std::cout << "\nТаблица отношений в индексах жителей:" << std::endl;
 	for (int i = 0; i < *relationsCount; i++){
-        cout << parents[i] << " " << children[i] << "\n" << endl;
+        std::cout << (*parents)[i] << " " << (*children)[i] << std::endl;
     }
-	
+
 }
 
-void PrintAllRelatives(string* people, int* peopleSets, int peopleCount)
+void PrintAllRelatives(std::vector<std::string>* people, std::vector<int>* peopleSets, int peopleCount)
 {
-	cout << "\nРезультат работы алгоритма поиска непересекающихся множеств:\n" << endl;
-	
+	std::cout << "\nРезультат работы алгоритма поиска непересекающихся множеств:" << std::endl;
+
 	for (int i = 0; i < peopleCount; i++)
-        cout << peopleSets[i] << " " + people[i] << endl;
-        
-	cout << "\nВсе пары родственников:\n" << endl;
-	
+        std::cout << (*peopleSets)[i] << " " + (*people)[i] << std::endl;
+
+	std::cout << "\nВсе пары родственников:" << std::endl;
+
 	for (int i = 0; i < peopleCount-1; i++)
 	{
 		for (int j = i+1; j < peopleCount; j++)
 		{
-			if (peopleSets[i] == peopleSets[j])
-				cout << people[i] + " - " + people[j] << endl;
+			if ((*peopleSets)[i] == (*peopleSets)[j])
+				std::cout << (*people)[i] + " - " + (*people)[j] << std::endl;
 		}
 	}
-	
+
 }
 
 int main()
 {
-	string* people = new string[SIZE]; //список всех имен
-	
-	int* peopleSets = new int[SIZE];  //массив, хранящий для каждой вершины дерева её непосредственного предка
-	int* peopleSetsRanks = new int[SIZE];  // для каждого дерева будет храниться верхняя граница его высоты
+	//string* people = new string[SIZE]; //список всех имен
 
-	int* parents = new int[SIZE];   //индексы имен из people в двух массивах показывают родство
-	int* children = new int[SIZE];
+	std::vector<std::string> people;
+
+	std::vector<int> peopleSets;  //массив, хранящий для каждой вершины дерева её непосредственного предка
+	std::vector<int> peopleSetsRanks;  // для каждого дерева будет храниться верхняя граница его высоты
+
+	std::vector<int> parents;   //индексы имен из people в двух массивах показывают родство
+	std::vector<int> children;
 	int peopleCount = 0;  //количество жителей
 	int relationsCount = 0;  //количество связей
 
-	ReadAndWritePeople(people, parents, children, &peopleCount, &relationsCount);  //считывание имен и заполнение связей родства
+	ReadAndWritePeople(&people, &parents, &children, &peopleCount, &relationsCount);  //считывание имен и заполнение связей родства
 
-    for (int i = 0; i < relationsCount; i++)  //для каждого элемента X, создать множество размера 1 из самого себя.
+    for (int i = 0; i < peopleCount; i++)  //для каждого элемента X, создать множество размера 1 из самого себя.
     {
-        peopleSets[i] = i;
-        peopleSetsRanks[i] = 0;
+        peopleSets.push_back(i);
+        peopleSetsRanks.push_back(0);
     }
 
 	for (int i = 0; i < relationsCount; i++)  //объединить два множества, в которых лежат элементы X и Y, в одно новое
 	{
-		UnionBranch(parents[i], children[i], peopleSets, peopleSetsRanks);
+		UnionBranch(parents[i], children[i], &peopleSets, &peopleSetsRanks);
     }
-    for (int i = 0; i < relationsCount; i++) //возвратить идентификатор множества, которому принадлежит элемент X
+    for (int i = 0; i < peopleCount; i++) //возвратить идентификатор множества, которому принадлежит элемент X (обновляем)
 	{
-		peopleSets[i] = GetRoot(i, peopleSets);
+		peopleSets[i] = GetRoot(i, &peopleSets);
     }
 
-	PrintAllRelatives(people, peopleSets, peopleCount); //печать пар родственников
+	PrintAllRelatives(&people, &peopleSets, peopleCount); //печать пар родственников
 
 	return 0;
 }
