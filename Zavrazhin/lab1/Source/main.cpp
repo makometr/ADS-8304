022c7576f4f4c8d02f5c443702b8ae6b8db0cc65
Daniyyel <zdg1999@gmail.com>
2019-09-23 19:32:28 +0300

    lab1 - Zavrazhin - var26 - correction

--- Zavrazhin/lab1/Source/main.cpp
+++ Zavrazhin/lab1/Source/main.cpp
@@ -1,331 +1,335 @@
-#include <cstdlib>  // for memory reallocation
-#include <iostream> // for basic input-output functionality
-#include <fstream>	// for file input-output functionality
-#include <string>   // for storage of lines read from a file
-#include <sstream>	// for conversion from a string to long long unsigned
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-constexpr size_t initial_sequence_capacity = 10;
-constexpr bool print_recursion_information = true;
+constexpr size_t INITIAL_SEQUENCE_CAPACITY = 10;
+constexpr bool PRINT_RECURSION_INFORMATION = true;
 
 
 template<class T>
-struct Sequence
+class Sequence
 {
-	size_t size = 0;
-	size_t capacity = initial_sequence_capacity;
+public:
+    std::vector<T> vector;
 
-	T *array = new T[initial_sequence_capacity];
+    // overload assignment operator
+    Sequence<T> &operator=(const Sequence<T> &sequence)
+    {
+        this->vector = sequence.vector;
+        return *this;
+    }
 
-	~Sequence()
-	{
-		delete [] array;
-	}
+    // overload
+    T operator[](size_t index)
+    {
+        return this->vector.at(index);
+    }
 
-	// overload assignment operator
-	void operator=(const Sequence<T> &seq)
-	{
-		delete [] array;
-		this->size = seq.size;
-		this->capacity = seq.capacity;
-		this->array = new T[this->capacity];
-		for(size_t i = 0; i < this->size; i++)
-			this->array[i] = seq.array[i];
-	}
-	// append the given element(s)
-	Sequence<T> &append(T elem)
-	{
-		if(this->size >= this->capacity)
-		{
-			this->capacity *= 2;
-			T *tmp_array = (T *) realloc(this->array, this->capacity * sizeof(T));
-			if(tmp_array == nullptr)
-				throw std::bad_alloc();
-			this->array = tmp_array;
-		}
-		this->array[this->size] = elem;
-		++this->size;
-		return *this;
-	}
+    // append the given element(s)
+    Sequence<T> &append(T elem)
+    {
+        this->vector.push_back(elem);
+        return *this;
+    }
 
-	Sequence<T> &append(const Sequence<T> &elems)
-	{
-		for(size_t i = 0; i < elems.size; ++i)
-			this->append(elems.array[i]);
-		return *this;
-	}
+    Sequence<T> &append(const Sequence<T> &elems)
+    {
+        this->vector.insert(this->vector.end(), elems.vector.begin(), elems.vector.end());
+        return *this;
+    }
 
-	template<typename... Ts>
-	Sequence<T> &append(T elem, Ts...elems)
-	{
-		this->append(elem);
-		this->append(elems...);
-		return *this;
-	}
+    // parameter packs are used to concisely append to a sequence, as in line 169
+    template<typename... Ts>
+    Sequence<T> &append(T elem, Ts...elems)
+    {
+        this->append(elem);
+        this->append(elems...);
+        return *this;
+    }
 
-	template<typename... Ts>
-	Sequence<T> &append(const Sequence<T> &elems1, Ts...elems2)
-	{
-		this->append(elems1);
-		this->append(elems2...);
-		return *this;
-	}
+    template<typename... Ts>
+    Sequence<T> &append(const Sequence<T> &elems1, Ts...elems2)
+    {
+        this->append(elems1);
+        this->append(elems2...);
+        return *this;
+    }
 
-	//overload the costructor to construct a sequence with provided elements 
-	Sequence(){}
+    //overload the costructor to construct a sequence with provided elements
+    Sequence(){}
 
-	Sequence(T elem)
-	{
-		this->append(elem);
-	}
+    Sequence(T elem)
+    {
+        this->append(elem);
+    }
 
-	Sequence(const Sequence<T> &elems)
-	{
-		this->append(elems);
-	}
+    Sequence(const Sequence<T> &elems)
+    {
+        this->append(elems);
+    }
 
-	template<typename... Ts>
-	Sequence(T elem, Ts...elems)
-	{
-		this->append(elem, elems...);
-	}
+    // some convenient member functions
 
-	template<typename... Ts>
-	Sequence(const Sequence<T> &elems1, Ts...elems2)
-	{
-		Sequence();
-		this->append(elems1, elems2...);
-	}
+    size_t cardinality()
+    {
+        return this->vector.size();
+    }
 
-	// some convenient member functions
-	Sequence<T> left_third(size_t pos)
-	{
-		Sequence<T> result;
-	// 1 here assures that the sequence-initial element
-	// is not considered to be a part of the left segment
-		for(size_t i = 1; i < pos; ++i)
-			result.append(this->array[i]);
-		return result;
-	}
+    Sequence<T> leftThird(size_t pos)
+    {
+        Sequence<T> result;
+        // 1 here assures that the sequence-initial element
+        // is not considered to be a part of the left segment
+        for(size_t i = 1; i < pos && i < this->vector.size(); ++i)
+                result.append(this->vector[i]);
+        return result;
+    }
 
-	Sequence<T> right_third(size_t pos)
-	{
-		Sequence<T> result;
-		for(size_t i = pos + 1; i < this->size; ++i)
-			result.append(this->array[i]);
-		return result;
-	}
+    Sequence<T> rightThird(size_t pos)
+    {
+        Sequence<T> result;
+        for(size_t i = pos + 1; i < this->vector.size(); ++i)
+                result.append(this->vector[i]);
+        return result;
+    }
 
-	// print the visual sequence representation
-	void print()
-	{
-		cout << '{';
-		if(this->size > 0)
-			cout << this->array[0];
-		for(size_t i = 1; i < this->size; i++)
-			cout << ", " << this->array[i];
-		cout << '}';
-	}
+    // print the visual sequence representation
+    std::string represent()
+    {
+        std::string result = "{";
+        if(this->vector.size() > 0)
+        {
+            result += std::to_string(this->vector[0]);
+            if(this->vector.size() > 1)
+                for(auto current = this->vector.begin() + 1, end = this->vector.end();
+                    current != end; ++current)
+                        result += ", " + std::to_string(*current);
+        }
+        return  result + "}";
+    }
 };
 
 
-Sequence<long long signed> Phi(Sequence<long long unsigned> &seq)
+Sequence<long long> computePhi(Sequence<long long unsigned> &sequence)
 {
-	// the depth will be tracked using a static variable
-	static size_t depth = 0;
-	++depth;
-	if(print_recursion_information)
-	{
-		cout << std::string(depth - 1, ' ') << "|-----------" << endl;
-		cout << std::string(depth - 1, ' ') << "| Phi was called with seq = ";
-		seq.print();
-		cout << endl;
-		cout << std::string(depth - 1, ' ') << "| depth: " << depth << endl;
-		cout << std::string(depth - 1, ' ') << "| cardinality : " << seq.size << endl;
-	}
-	
-	Sequence<long long signed> result;
-	// in the case of an empty sequence, {0} shall be returned
-	if(seq.size == 0)
-	{
-		if(print_recursion_information)
-			cout << std::string(depth - 1, ' ') << "| empty sequence case" << endl;
-		result.append(0);
-	}
-	// in the case of a sequence containing a single value,
-	// a sequence containing only the one opposite to it shall be returned
-	else if(seq.size == 1)
-	{
-		if(print_recursion_information)
-			cout << std::string(depth - 1, ' ') << "| sole element case" << endl;
-		result.append(-(long long)seq.array[0]);
-	}
-	else
-	{
-		// the program shall check whether the first number in the given sequence
-		// is divisible by at least one another
-		bool div = false;
-		size_t pos = 1;
-		for(size_t i = 1; i < seq.size && !div; ++i)
-			if(seq.array[0] % seq.array[i] != 0)
-				++pos;
-			else
-				div = true;
-		if(div)
-		{
-			if(print_recursion_information)
-			{
-				cout << std::string(depth - 1, ' ') << "| tripartite case" << endl;
-				cout << std::string(depth - 1, ' ') << "| pos  : " << pos << endl;
-			}
-			auto left_third = seq.left_third(pos);
-			auto right_third = seq.right_third(pos);
-			result.append(Phi(left_third));
-			result.append(999, seq.array[0] + seq.array[pos]);
-			result.append(Phi(right_third));
-		}
-		else
-		{
-			if(print_recursion_information)
-				cout << std::string(depth - 1, ' ') << "| 666 case" << endl;
-			result.append(666);
-		}
-	}
-	if(print_recursion_information)
-	{
-		cout << std::string(depth - 1, ' ') << "|-----------" << endl;
-		cout << std::string(depth - 1, ' ') << "| depth: " << depth << endl;
-		cout << std::string(depth - 1, ' ') << "| Phi exited with value ";
-		result.print();
-		cout << endl;
-	}
-	--depth;
-	if(depth == 0 && print_recursion_information)
-		cout << "|-----------" << endl;
-	return result;
-}
+    // the depth will be tracked using a static variable
+    static size_t depth = 0;
+    depth += 1;
+    if(PRINT_RECURSION_INFORMATION)
+    {
+        cout << std::string(depth - 1, ' ') << "|-----------" << endl;
+        cout << std::string(depth - 1, ' ') << "| computePhi was called with seq = " << sequence.represent() << endl;
+        cout << std::string(depth - 1, ' ') << "| depth: " << depth << endl;
+        cout << std::string(depth - 1, ' ') << "| cardinality : " << sequence.cardinality() << endl;
+    }
 
-void readFromFile(const std::string &file_path)
-{
-	// used to output debug information
-	size_t line_counter = 0;
-	
-	std::ifstream file(file_path);
-
-	// each line in a file is interpreted to contain a sequence
-	if (file.is_open())
-	{
-		for(std::string line; getline(file, line);)
-		{
-			cout << endl;
-			cout << "Line " << line_counter << endl;
-			++line_counter;
-			Sequence<long long unsigned> seq;
-			long long unsigned number;
-
-			auto current = line.begin();
-			auto end = line.end();
-			std::string number_representation = "";
-
-			if(current == end)
-			{
-				cout << "Error: line " << line_counter << " happens to be empty." << endl;
-				continue;
-			}
-			if(*current != '{')
-			{
-				cout << "Error: line " << line_counter << " does not start with '{'." << endl;
-				continue;
-			}
-			++current;
-			
-			bool curly_bracket_encountered = false;
-			while(current != end)
-			{
-				if(std::string("0123456789").find(*current) != std::string::npos)
-				{
-					number_representation += *current;
-				}
-				else if(*current == ',')
-				{
-					if(number_representation.length() > 0)
-					{
-						std::stringstream(number_representation) >> number;
-						if(number == 0)
-						{
-							cout << "Error: line " << line_counter << " contains the number 0, which is not a natural number." << endl;
-							goto new_line;
-						}
-						seq.append(number);
-						number_representation = "";
-					}
-				}
-				else if(*current == '}')
-				{
-					if(number_representation.length() > 0)
-					{
-						std::stringstream(number_representation) >> number;
-						if(number == 0)
-						{
-							cout << "Error: line " << line_counter << " contains the number 0, which is not a natural number." << endl;
-							goto new_line;
-						}
-						seq.append(number);
-						number_representation = "";
-					}
-					curly_bracket_encountered = true;
-					break;
-				}
-				else if(*current == ' ' || *current == '\t')
-				{
-					if(number_representation.length() > 0)
-					{
-						std::stringstream(number_representation) >> number;
-						if(number == 0)
-						{
-							cout << "Error: line " << line_counter << " contains the number 0, which is not a natural number." << endl;
-							goto new_line;
-						}
-						seq.append(number);
-						number_representation = "";
-					}
-				}
-				else
-				{
-					cout << "Warning: line " << line_counter << " contains an unsupported character '" << *current << "' which will be ignored." << endl;
-				}
-				++current;
-			}
-			if(!curly_bracket_encountered)
-			{
-				cout << "Error: line " << line_counter << " happens to lack '}'." << endl;
-				continue;
-			}
-			cout << "The following sequence was acquired:" << endl;
-			seq.print();
-			cout << endl;
-			cout << "Result:" << endl;
-			Phi(seq).print();
-			cout << endl;
-			new_line:
-			continue;
-		}
-	}
-	else
-	{
-		cout << "Unfortunately, file could not be opened." << endl;
-	}
+    Sequence<long long> result;
+    // in the case of an empty sequence, {0} shall be returned
+    if(sequence.cardinality() == 0)
+    {
+        if(PRINT_RECURSION_INFORMATION)
+            cout << std::string(depth - 1, ' ') << "| empty sequence case" << endl;
+        result.append(0);
+    }
+    // in the case of a sequence containing a single value,
+    // a sequence containing only the one opposite to it shall be returned
+    else if(sequence.cardinality() == 1)
+    {
+        if(PRINT_RECURSION_INFORMATION)
+            cout << std::string(depth - 1, ' ') << "| sole element case" << endl;
+        result.append(-(long long)sequence[0]);
+    }
+    else
+    {
+        // the program shall check whether the first number in the given sequence
+        // is divisible by at least one another
+        bool isDivisible = false;
+        size_t pos = 1;
+        for(size_t i = 1; i < sequence.cardinality() && !isDivisible; ++i)
+            if(sequence[0] % sequence[i] != 0)
+                pos += 1;
+            else
+                isDivisible = true;
+        if(isDivisible)
+        {
+            if(PRINT_RECURSION_INFORMATION)
+            {
+                cout << std::string(depth - 1, ' ') << "| tripartite case" << endl;
+                cout << std::string(depth - 1, ' ') << "| pos  : " << pos << endl;
+            }
+            auto leftThird = sequence.leftThird(pos);
+            auto rightThird = sequence.rightThird(pos);
+            result.append(computePhi(leftThird), 999, sequence[0] + sequence[pos], computePhi(rightThird));
+        }
+        else
+        {
+            if(PRINT_RECURSION_INFORMATION)
+                cout << std::string(depth - 1, ' ') << "| 666 case" << endl;
+            result.append(666);
+        }
+    }
+    if(PRINT_RECURSION_INFORMATION)
+    {
+        cout << std::string(depth - 1, ' ') << "|-----------" << endl;
+        cout << std::string(depth - 1, ' ') << "| depth: " << depth << endl;
+        cout << std::string(depth - 1, ' ') << "| computePhi exited with value " << result.represent() << endl;
+    }
+    depth -= 1;
+    if(depth == 0 && PRINT_RECURSION_INFORMATION)
+        cout << "|-----------" << endl;
+    return result;
 }
 
 
-int main()
+
+void readFromFile(const std::string &filePath)
 {
-	std::string file_path = "";
-	cout << "Enter path to a file containing test cases." << endl;
-	cin >> file_path;
-	readFromFile("tests.txt");
-	return 0;
+    // used to output debug information
+    size_t lineCounter = 0;
+
+    std::ifstream file(filePath);
+
+    // each line in a file is interpreted to contain a sequence
+    if (file.is_open())
+    {
+        for(std::string line; getline(file, line);)
+        {
+            cout << endl;
+            lineCounter += 1;
+            cout << "Line " << lineCounter << endl;
+            Sequence<long long unsigned> seq;
+            long long unsigned number;
+
+            auto current = line.begin();
+            auto end = line.end();
+            std::string numberRepresentation = "";
+
+            if(current == end)
+            {
+                cout << "Error: line " << lineCounter << " happens to be empty." << endl;
+                continue;
+            }
+            if(*current != '{')
+            {
+                cout << "Error: line " << lineCounter << " does not start with '{'." << endl;
+                continue;
+            }
+            current += 1;
+
+            bool curly_bracket_encountered = false;
+            while(current != end)
+            {
+                if(0x30 <= *current && *current <= 0x39) // i.e. if it is a digit
+                {
+                    numberRepresentation += *current;
+                }
+                else if(*current == ',')
+                {
+                    if(numberRepresentation.length() > 0)
+                    {
+                        std::stringstream(numberRepresentation) >> number;
+                        if(number == 0)
+                        {
+                            cout << "Error: line " << lineCounter
+                                 << " contains the number 0, which is not a natural number."
+                                 << endl;
+                            goto new_line;
+                        }
+                        seq.append(number);
+                        numberRepresentation = "";
+                    }
+                }
+                else if(*current == '}')
+                {
+                    if(numberRepresentation.length() > 0)
+                    {
+                        std::stringstream(numberRepresentation) >> number;
+                        if(number == 0)
+                        {
+                            cout << "Error: line " << lineCounter
+                                 << " contains the number 0, which is not a natural number."
+                                 << endl;
+                            goto new_line;
+                        }
+                        seq.append(number);
+                        numberRepresentation = "";
+                    }
+                    curly_bracket_encountered = true;
+                    break;
+                }
+                else if(*current == ' ' || *current == '\t')
+                {
+                    if(numberRepresentation.length() > 0)
+                    {
+                        std::stringstream(numberRepresentation) >> number;
+                        if(number == 0)
+                        {
+                            cout << "Error: line " << lineCounter
+                                 << " contains the number 0, which is not a natural number."
+                                 << endl;
+                            goto new_line;
+                        }
+                        seq.append(number);
+                        numberRepresentation = "";
+                    }
+                }
+                else
+                {
+                    cout << "Warning: line " << lineCounter << " contains an unsupported character '"
+                         << *current << "' which will be ignored." << endl;
+                }
+                current += 1;
+            }
+            if(!curly_bracket_encountered)
+            {
+                cout << "Error: line " << lineCounter << " happens to lack '}'." << endl;
+                continue;
+            }
+            cout << "The following sequence was acquired:" << endl;
+            cout << seq.represent() << endl;
+            cout << "Result:" << endl;
+            cout << computePhi(seq).represent() << endl;
+            new_line:
+            continue;
+        }
+    }
+    else if(!file.good())
+    {
+         cout << "The specified path is incorrect or leads to a folder" << endl;
+    }
+    else
+    {
+         cout << "Unfortunately, file could not be opened." << endl;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    std::string filePath = "";
+    if(argc == 2)
+    {
+        filePath = std::string(argv[1]);
+        cout << "Specified path:" << endl;
+        cout << filePath << endl;
+    }
+    else if(argc > 2)
+    {
+        cout << "Too many command line arguments" << endl;
+    }
+    if(argc != 2)
+    {
+        cout << "Enter path to a file containing input sequences" << endl;
+        cout << "(In is possible to specify it as a sole command line argument)." << endl;
+        cout << "The file provided with the program is located in \"Test/tests.txt\"" << endl;
+        cin >> filePath;
+    }
+    readFromFile(filePath);
+    return 0;
 }
