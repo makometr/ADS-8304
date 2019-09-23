7cde081e25b2f06eec419529b153fcda50301b24
gagask <kholkovskii@mail.ru>
2019-09-24 01:45:29 +0300

    Permanent Fix

--- Holkovskii/lab2/Source/bery.cpp
+++ Holkovskii/lab2/Source/bery.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 #include <map>
 
 struct Node{
@@ -31,45 +32,42 @@ struct Node{
 			if (std::get<std::string>(val) == "NOT")
 				return ! std::get<Node*>(argv)->evaluate();
 		}
-		else
-			return std::get<int>(val);
+		return std::get<int>(val);
 	}
 
 	std::variant<std::pair<Node*, Node*>, Node*> argv;
 	std::variant<int, std::string> val;               //std::variant<bool,std::string> val;
 };
 
-typedef struct Node Node;
-
-void func(Node*&, std::string const&, std::map<std::string,bool> const&);
+void mkNode(Node*&, std::string const&, std::map<std::string,bool> const&);
 
 void mkAtom(std::string const& str,int& ind,Node*& arg, std::map<std::string,bool> const& dict){
 	if (str[ind] == '(')
 	{
-		int tmp = ind, error = 0;
-		std::string tmp_s = "";
+		int new_ind = ind, error = 0;
+		std::string new_str = "";
 		while (1)                //нахождение соот-ей ')'
 		{
-			tmp_s += str[tmp];
-			tmp++;
-			if (str[tmp] == '(')
+			new_str += str[new_ind];
+			new_ind++;
+			if (str[new_ind] == '(')
 				error++;
-			if (str[tmp] == ')')
+			if (str[new_ind] == ')')
 				error--;
 			if (error < 0)
 				break;
 		}
-		tmp_s += str[tmp];
-		func(arg, tmp_s, dict);
-		ind = tmp + 1;
+		new_str += str[new_ind];
+		mkNode(arg, new_str, dict);
+		ind = new_ind + 1;
 	}
 	else
 	{
 		std::string tmp = "";
 		while (str[ind] != ' ' && str[ind] != '(' && str[ind] != ')')
 			tmp += str[ind++];
-		auto tr=dict.find(tmp);
-		if(tr==dict.end())
+		auto tr = dict.find(tmp);
+		if(tr == dict.end())
 		{
 			std::cout<<tmp<<" - not declared"<<std::endl;
 			exit(1);
@@ -78,28 +76,30 @@ void mkAtom(std::string const& str,int& ind,Node*& arg, std::map<std::string,boo
 	}
 }
 
-void func(Node* & point, std::string const& str, std::map<std::string,bool> const& dict)
+void mkNode(Node* & point, std::string const& str, std::map<std::string,bool> const& dict)
 {
         int ind = 0;
 	Node* arg1 = new Node;
 	Node* arg2 = new Node;
 	while (str[ind] == '(' || str[ind] == ' ')//перейти к оператору
 		ind++;
-	std::string oper="";                      //
-	while(str[ind] != ' ' && str[ind] != '(') //}считывание оператора
-		oper+=str[ind++];                 //
-	if (oper != "AND" && oper != "OR" && oper != "NOT" && oper != "XOR")//проверка оператора
+	std::string name_op = "";
+	while(str[ind] != ' ' && str[ind] != '(')
+		name_op += str[ind++];
+	if (name_op != "AND" && name_op != "OR" && name_op != "NOT" && name_op != "XOR")
 	{
 		std::cout << "Invalid operator!\n";
 		delete arg1;
 		delete arg2;
 		exit(1);
 	}
-	point->val = oper;             //запись оператора
-	while(str[ind] == ' '){ind++;} //переход к первому аргументу или скобочке
+	point->val = name_op;
+	while(str[ind] == ' ')
+		ind++;
 	mkAtom(str, ind, arg1, dict);
-	while (str[ind] == ' '){ind++;}//переход к следующему аргументу или скобочке
-	if (str[ind] == ')')           //проверка унарности
+	while (str[ind] == ' ')
+		ind++;
+	if (str[ind] == ')')
 	{
 		if (std::get<std::string>(point->val) == "NOT")
 		{
@@ -118,56 +118,87 @@ void func(Node* & point, std::string const& str, std::map<std::string,bool> cons
 }
 
 int main(int argc, char* argv[]){
-	if(argc==1)
+	if(argc == 1)
 	{
 		std::map<std::string,bool> dict;
-		char* key=new char[20];
+		std::string s, key = "";
 		int val;
-		std::cout<<"Введите список значений переменных"<<std::endl;
-		scanf("(");
-		while(scanf("(%s %d)",key,&val)==2)
-			dict.insert({key,val});
-		delete[] key;
-		std::string s;
-		std::getline(std::cin,s);
-		std::cout<<"Введите выражение"<<std::endl;
-		std::getline(std::cin,s);
+		std::cout << "Введите список значений переменных" << std::endl;
+		std::getline(std::cin, s);
+		s.erase(s.begin());
+		s.erase(s.end()-1);
+		int tmp = 0;
+		for(size_t i = 0; i < s.size(); ++i)
+			if (s[i] == '(')
+				tmp++;
+		for(int i = 0; i < tmp; ++i)
+		{
+			int k = 1;
+			std::string tmp_s = "";
+			while(s[k] != ' ')
+				tmp_s += s[k++];
+			k++;
+			key = tmp_s;
+			tmp_s = "";
+			while(s[k] != ')')
+				tmp_s += s[k++];
+			std::istringstream stream(tmp_s);
+			stream >> val;
+			dict.insert({key, val});
+			s.erase(0, ++k);
+		}
+		std::cout << "Введите выражение" << std::endl;
+		std::getline(std::cin, s);
 		Node* point = new Node;
-		func(point, s, dict);
+		mkNode(point, s, dict);
 		std::cout << point->evaluate() << std::endl;
 		delete point;
 	}
 	else
 	{
-		std::cout<<"For filename:"<<argv[1]<<std::endl;
+		std::cout << "For filename:" << argv[1] << std::endl;
 		std::ifstream in(argv[1]);
 		if (!in.is_open()){
-			std::cout<<"Can't open file"<<std::endl;
+			std::cout << "Can't open file" << std::endl;
 			return 0;
 		}
 		if (in.eof()){
-			std::cout<<argv[1]<<"is empty File"<<std::endl;
+			std::cout << argv[1] << "is empty File" << std::endl;
 			return 0;
 		}
-		FILE* file = fopen(argv[1], "r");
 		std::map<std::string,bool> dict;
-		char* key=new char[20];
-		int val;
-		fscanf(file,"(");
-		while(fscanf(file,"(%s %d)",key,&val)==2)
-			dict.insert({key,val});
-		delete[] key;
-		fclose(file);
-		std::string s;
-		std::getline(in,s);
-		std::getline(in,s);
+		std::string s, key="";
+                int val;
+                std::getline(in, s);
+                s.erase(s.begin());
+                s.erase(s.end()-1);
+                int tmp = 0;
+                for(size_t i = 0; i < s.size(); ++i)
+                        if (s[i] == '(')
+                                tmp++;
+                for(int i = 0; i < tmp; ++i)
+                {
+                        int k = 1;
+                        std::string tmp_s = "";
+                        while(s[k] != ' ')
+                                tmp_s += s[k++];
+                        k++;                                                                                                                                                                                                               key=tmp_s;
+                        tmp_s = "";
+                        while(s[k] != ')')
+                                tmp_s += s[k++];
+                        std::istringstream stream(tmp_s);
+                        stream >> val;
+                        dict.insert({key, val});
+                        s.erase(0, ++k);
+                }
+		std::getline(in, s);
 		Node* point = new Node;
 		std::cout<<"Для значений: "<<std::endl;
 		for(auto tr = dict.begin();tr != dict.end(); ++tr)
-			std::cout<<tr->first<<" : "<<tr->second<<std::endl;
-		std::cout<<"И для выражения: "<<s<<std::endl;
-		func(point, s, dict);
-		std::cout <<"Ответ: "<< point->evaluate() << std::endl;
+			std::cout << tr->first << " : " << tr->second << std::endl;
+		std::cout << "И для выражения: " << s << std::endl;
+		mkNode(point, s, dict);
+		std::cout << "Ответ: " << point->evaluate() << std::endl;
 		delete point;
 	}
 	return 0;
