82496e2b2ae6ec356c8a86de9167474f94b11e52
Pavel377dq <pad93765@gmail.com>
2019-09-23 18:35:19 +0300

    fixed all

--- lab1/Source/lr1.cpp
+++ lab1/Source/lr1.cpp
@@ -4,182 +4,272 @@
 #include<string>//getline()
 #include <sstream>//istringstream()
 #include<fstream>// ifstrem()
+#include<cctype>
+
+
 
 
 using namespace std;
 
-
-class Line {
-	vector<int> _data;//vector стандартный шаблон C++ инициализируем пустой вектор вместо int можно поставить свой класс
-public:
-	const vector<int>& data() const//возвращаем вектор типа int.Возвращаем адрес вектора?
-	{
-		return _data;
-	}
-	friend istream& operator>>(istream& is, Line& line);//istream класс
-	friend ostream& operator<<(ostream& os, const Line& line);
-};
-
-ostream& operator<<(ostream& os, const Line& line)//тип возвращаемого значения - объект класса ostream
-{
-	copy(line._data.begin(), line._data.end(), ostream_iterator<int>{os, " "});//копирует из одного контейнера в другой здесь другой это os, класс шаблона ostream_iterator описывает объект итератора вывода
-
-	return os;
-}
+void RecF(vector<int>& vectorin, string* vectorout, int ident, int length) {
 
 
-istream& operator>>(istream& is, Line& line)
-{
-	string str;
-	getline(is, str);
-	istringstream ss{ str };//Превращаем строку в поток
-	line._data.assign(istream_iterator<int>{ss}, {});//istream_iterator<int>{data} читать последователность типа int (делитель " ")
-
-	return is;
-}
 
 
-void RecF(vector<int> &vectorin, string* vectorout, int ident, int length) {
 
-	
-	int i ,j;
+	int i, j;
 
-	for ( i = 0; i < ident; i++)
+
+
+	for (i = 0; i < ident; i++)
+
 		cout << "\t";
+
+
 
 	cout << "RecF(";
 
-	for (i = 0; i < length; i++)
-		cout << vectorin[i]<<" ";
 
-	cout << ")" <<endl;
-	
+
+	for (i = 0; i < length; i++)
+
+		cout << vectorin[i] << " ";
+
+
+
+	cout << ")" << endl;
+
+
+
 	if (length % 2 == 0 && length != 2) {
 
 		vector<int> vectorcopy1(length / 2);
 		vector<int> vectorcopy2(length / 2);
-	
+
 		for (i = 0; i < length / 2; i++)
+
 			vectorcopy1[i] = vectorin[i];
 
 		RecF(vectorcopy1, vectorout, ident + 1, length / 2);
 		j = 0;
 
 		for (i = length / 2; i < length; i++) {
+
 			vectorcopy2[j] = vectorin[i];
 			j++;
 		}
 
 		RecF(vectorcopy2, vectorout, ident + 1, length / 2);
 
-	//	cout << "len" << length<<endl;
 	}
+
 	else if (length % 2 != 0 && length != 1) {
-		//cout << "len" << length<<endl;
+
 		vector<int> vectorcopy1((length / 2) + 1);
 		vector<int> vectorcopy2((length / 2) + 1);
+
+
 
 		for (i = 0; i < (length / 2) + 1; i++)
 			vectorcopy1[i] = vectorin[i];
 
 		RecF(vectorcopy1, vectorout, ident + 1, (length / 2) + 1);
 
-		j = 0;//индекс для корректного копирования в новый массив
+		j = 0;
 
-		for (i = (length / 2) ; i < length; i++) {
+		for (i = (length / 2); i < length; i++) {
+
 			vectorcopy2[j] = vectorin[i];
 			j++;
+
 		}
-		//for (i = 0; i < (length / 2) + 1; i++)
-			//cout << vectorcopy2[i];
+
 		RecF(vectorcopy2, vectorout, ident + 1, (length / 2) + 1);
 
 
 	}
+
 	else if (length == 2) {
 
-		//cout << "len2" << endl;
-		*vectorout += to_string(vectorin[0]);
-		*vectorout += " ";
-		*vectorout += to_string(vectorin[1]);
-		*vectorout += " ";
-	}
-	else if(length == 1) {
 
-		*vectorout+= to_string(vectorin[0]);
+		stringstream ss1;
+		ss1<<vectorin[0];
+		*vectorout += ss1.str();
+
+		*vectorout += " ";
+
+		stringstream ss2;
+		ss2<<vectorin[1];
+		*vectorout += ss2.str();
+
+		*vectorout += " ";
+
 	}
+
+	else if (length == 1) {
+		stringstream ss1;
+		ss1<<vectorin[0];
+		*vectorout += ss1.str();
+
+	}
+
+
+
+
+
+}
+
+
+
+int is_alldigits(vector<string> is_digits){//проверяет все ли числа в строке
+
+	//istringstream ss{ is_digits };
+      	//vector<string> istr_digit;
+      	//istr_digit.assign(istream_iterator<string>{s1},{});
+	int flag = 1;
+      	for(int i = 0; i < is_digits.size(); i++)//0 если не число
+        	if(is_digits[i][0] != '-'){
+                	for(int j = 0; j < is_digits[i].length(); j++)
+                        	if(isdigit(is_digits[i][j]) == 0)
+                                	flag = 0;
+                }
+       		else{
+                        for(int j = 1; j < is_digits[i].length(); j++)
+                        	if(isdigit(is_digits[i][j]) == 0)
+                                        flag = 0;
+                }
 	
-	
+	return flag;
+
 }
 
 
 
 
-int main(){
+
+
+int main() {
+
+
 
 	setlocale(LC_ALL, "Russian");
 	string vector_out;
 	vector_out = "";
 	int ident = 0;
-	int flag = 0;
+	int flag;
 
-	cout << "Ввод из файла или из консоли? (f , c)?\n";
+
+
+	cout << "Ввод из файла или из консоли? (f , c)\n";
+
 	char arg;
 	cin >> arg;
+
 	if (arg == 'f') {
 
-		ifstream data("C:\\Users\\Павел\\source\\repos\\ConsoleApplication1\\Debug\\input\\inp.txt");
 
-		vector<Line> vectors_inp(istream_iterator<Line>{data}, {});//istream_iterator<Line>{data} - читать последовательность типа Line (те каждая строка имеет тип Line, а разделитель \n, объект класса Line - data
+
+		ifstream data("inp.txt");
+
+		ofstream fout("out.txt");
 		
-	
-		if (!data) {
+		if(data){
 
-			int len = vectors_inp.size();
+			string str;
 
-			for (int i = 0; i < len; i++) {
-				vector<int> vector_inp = vectors_inp[i].data();
-				
+			while (getline(data, str)) {
+
+				istringstream s1{ str };
+				vector<string> is_digits;
+				is_digits.assign(istream_iterator<string>{s1},{});
+
+				flag =  is_alldigits(is_digits);
+
+				if(flag == 0){
+
+					cout<<"Данные во вновь обрабатываемой строке введены некорректно"<<endl;
+					fout<<"Данные во вновь обрабатываемой строке введены некорректно";
+
+					break;
+				}
+
+				istringstream s2{ str };
+				vector<int> vector_inp;
+				vector_inp.assign(istream_iterator<int>{s2}, {});//istream_iterator описывает обЪект итератора ввода, assign в классе vector  обобщенная функция
 				RecF(vector_inp, &vector_out, ident, vector_inp.size());
 				cout << vector_out << endl;
-				vector_out = "";
-				//cout << vectors_inp[i];
-			}
-
+				fout<<vector_out;
+				fout<<"\n";
 				
+				vector_out = "";
+		
+			}
 			
+			fout.close();
+			data.close();
 		}
 		else
-			cout << "Файл не может быть открыт";
+			cout<<"Файл не открыт"<<endl;
+		
+
 
 
 	}
+
 	else if (arg == 'c') {
 
-		
-		
+
 		string str;
-		getline(cin, str);//иначе читает пустую строку
+		getline(cin, str);
+
+		ofstream fout("out.txt");
+
+		while (getline(cin, str)) {
 
 
-		while(getline(cin, str)){
+			        istringstream s1{ str };
+                                vector<string> is_digits;
+                                is_digits.assign(istream_iterator<string>{s1},{});
 
-			istringstream ss{ str };//Превращаем строку в поток
+                                flag =  is_alldigits(is_digits);
+
+                                if(flag == 0){
+
+                                        cout<<"Данные во вновь обрабатываемой строке введены некорректно"<<endl;
+					fout<<"Данные во вновь обрабатываемой строке введены некорректно";
+
+                                        break;
+                                }
+
+			istringstream s2{ str };
 			vector<int> vector_inp;
-			vector_inp.assign(istream_iterator<int>{ss}, {});
-			//cout << vector_inp.size();
+			vector_inp.assign(istream_iterator<int>{s2}, {});
+
 			RecF(vector_inp, &vector_out, ident, vector_inp.size());
 			cout << vector_out << endl;
-			//Line vector_inpc;
+			fout<<vector_out;
+                        fout<<"\n";
+
 			vector_out = "";
+
 		}
-		
+
+		fout.close();
+               // data.close();
+
+
+
+
 	}
+
 	else
-		cout << "Такой команды нет";
-		
+
+		cout << "Нет такой команды";
+
+
+
 	return 0;
-    
+
+
+
 }
-
-
