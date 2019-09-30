9b1f0b7e28c17a02721146568d06cb7b620b716d
Mukhin Aleksandr <myxin200015@mail.ru>
2019-09-30 23:56:19 +0300

    git omg

--- Mukhin/lab3/lab3.sh
+++ Mukhin/lab3/lab3.sh
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-g++ Source/main.cpp Source/lab3.hpp -o lab3
-=======
-g++ -Wall -Werror Source/lab3.cpp -o lab3
->>>>>>> 5f44ba91f96a18f7e2eb2a55f2e7276c5f0c82dc
+g++ -Wall -Werror Source/main.cpp Source/lab3.hpp -o lab3
 ./lab3 ./Tests/in.txt ./Tests/out.txt
 rm lab3
+
