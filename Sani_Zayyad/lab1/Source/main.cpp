7f7768e4b71f1d2bebf668659109e92742f71190
Codedbyte <prinzezayd@gmail.com>
2019-10-02 00:50:10 +0300

    fixes made

--- Sani_Zayyad/lab1/Source/main.cpp
+++ Sani_Zayyad/lab1/Source/main.cpp
@@ -2,37 +2,31 @@
 #include <string>
 #include <fstream>
 
-//declaration of Functions
 void ProceedInput();
 std::string ReadFromConsole();
 void ReadFromFile(std::string filename);
-bool checkValidity(std::string str);
-std::string notFunction(std::string str);
-bool expression(std::string str, int pointer);
+bool checkBracket(std::string str);
+std::string recursiveNotExpression(std::string str);
+bool recursiveExpression(std::string str, int pointer);
 void calculate(std::string input);
 
-//this is the starting point of the progran
-// it asks user to select input option and uses the input to calculate the logic
+
 void ProceedInput()
 {
-    //dialog
     std::cout << "> Lab work #1" << std::endl;
     std::cout << "> Choose your input" << std::endl;
     std::cout << "> 0 - from console" << std::endl;
-    std::cout << "> 1 - from file default file (input.txt)" << std::endl;
+    std::cout << "> 1 - from file default file - /Documents/ADS-8304/Sani_Zayyad/lab1/Test/input.txt" << std::endl;
     std::cout << "> 2 - from custom file" << std::endl;
     std::cout << "> Any other key to Exit!" << std::endl;
 
     int command = 0;
-    //command input
     std::cin >> command;
     std::cin.ignore();
 
-    //default test file path
-    std::string FilePath = "/Users/sanizayyad/Documents/input.txt";
+    std::string FilePath = "/Users/sanizayyad/Documents/ADS-8304/Sani_Zayyad/lab1/Test/input.txt";
     std::string input;
 
-    // I used switch statement to trigger the action base on the dialog
     switch (command)
     {
     case 0:
@@ -62,23 +56,20 @@ std::string ReadFromConsole()
 //this function iterates the lines of the file and trigger calculate on every line as input
 void ReadFromFile(std::string filename)
 {
-    std::ifstream file;
-    file.open(filename);
+    std::ifstream file(filename);
 
     if (file.is_open())
     {
-        std::cout << "Reading from file:"
-             << "\n";
+        std::cout << "Reading from file:"<< "\n";
 
-        int super_count = 0;
+        int count = 0;
 
         while (!file.eof())
         {
-            super_count++;
+            count++;
             std::string str;
             getline(file, str);
-            std::cout << "test #" << super_count << " \"" + str + "\""
-                 << "\n";
+            std::cout << "test #" << count << " \"" + str + "\""<< "\n";
             calculate(str);
         }
     }
@@ -86,15 +77,17 @@ void ReadFromFile(std::string filename)
     {
         std::cout << "File not opened";
     }
-    file.close();
 }
 // checking if the brackets are balanced
-bool checkValidity(std::string str)
+bool checkBracket(std::string str)
 {
     int balanceBracket = 0;
-    int i = 0;
-    for (i; i < str.size(); i++)
+    for (int i = 0; i < str.size(); i++)
     {
+        if(str[0] == ')' || str[str.size()-1] == '('){
+            return false;
+        }
+        
         if (str[i] == ')')
             balanceBracket--;
         else if (str[i] == '(')
@@ -104,8 +97,8 @@ bool checkValidity(std::string str)
         return true;
     return false;
 }
-//the main recursive function to check expression
-bool expression(std::string str, int pos)
+//the main recursive function to check recursiveExpression
+bool recursiveExpression(std::string str, int pos)
 {
     if (str == "TRUE")
         return true;
@@ -119,14 +112,13 @@ bool expression(std::string str, int pos)
     if (str[pos] == '(')
     {
         pos++;
-        bool a, b;
-        std::string exp_1, exp_2, sign;
+        bool expression1, expression2;
+        std::string string_expression1, string_expression2, sign;
 
-        //I splitted the expression three which means for every expression there's
-        //expression1 Sign expression2//
+        //I splitted the recursiveExpression three which means for every recursiveExpression there's
+        //recursiveExpression1 Sign recursiveExpression2//
 
-        //checking the main demarcation between two expression
-        //for example (TRUE AND FALSE) , here the first space after TRUE is our demarcation
+    
         int breakspace = 0;
         int bracket = 0;
         for (breakspace = pos; breakspace < str.size(); breakspace++)
@@ -143,64 +135,72 @@ bool expression(std::string str, int pos)
         }
         int tmp = breakspace - 1;
 
-        //////expression 1
-        exp_1 = str.substr(pos, tmp);
-        //if there's a Not statement in expression1
-        //we call notFunction to calculate the NOT(logic)
-        if (exp_1.find("NOT") != std::string::npos)
+        //////Expression 1
+        string_expression1 = str.substr(pos, tmp);
+        //if there's a Not statement in Expression1
+        //we call recursiveNotExpression to calculate the NOT(logic)
+        if (string_expression1.find("NOT") != std::string::npos)
         {
-            exp_1 = notFunction(exp_1);
+            string_expression1 = recursiveNotExpression(string_expression1);
         }
+        
         //////SIGN
         std::string tmpStr = str.substr(tmp + 2);
         int tmp_2 = tmpStr.find(" ");
         sign = tmpStr.substr(0, tmp_2);
-        ////expression 2
-        exp_2 = tmpStr.substr(tmp_2 + 1, tmpStr.size() - tmp_2 - 2);
-        if (exp_2.find("NOT") != std::string::npos)
+        
+        ////Expression 2
+        string_expression2 = tmpStr.substr(tmp_2 + 1, tmpStr.size() - tmp_2 - 2);
+        if (string_expression2.find("NOT") != std::string::npos)
         {
-            exp_2 = notFunction(exp_2);
+            string_expression2 = recursiveNotExpression(string_expression2);
         }
 
-        //recursive on expression 1
-        a = expression(exp_1, 0);
-        // recursive on expression 2
-        b = expression(exp_2, 0);
+        //recursive on Expression 1
+        expression1 = recursiveExpression(string_expression1, 0);
+        // recursive on Expression 2
+        expression2 = recursiveExpression(string_expression2, 0);
 
         // the  we return the outcome
         if (sign == "AND")
-            return (a && b);
+            return (expression1 && expression2);
         else
-            return (a || b);
+            return (expression1 || expression2);
     }
 }
 //this returns the negation of a logic.
-std::string notFunction(std::string str)
+std::string recursiveNotExpression(std::string str)
 {
     int tmp = str.find("NOT");
-    std::string tmp_expression = str.substr(tmp + 3, str.size());
-    int tmp_closing = tmp_expression.find(")");
-    tmp_expression = tmp_expression.substr(0, tmp_closing);
+    std::string tmp_recursiveExpression = str.substr(tmp + 3, str.size());
+    int tmp_closing = tmp_recursiveExpression.find(")");
+    tmp_recursiveExpression = tmp_recursiveExpression.substr(0, tmp_closing);
     bool before;
 
-    if (tmp_expression.find(" ") != std::string::npos)
+    if (tmp_recursiveExpression.find(" ") != std::string::npos)
     {
-        before = expression(tmp_expression, 0);
+        before = recursiveExpression(tmp_recursiveExpression, 0);
     }
     else
     {
-        tmp_expression = tmp_expression.substr(1, tmp_closing - 1);
-        before = expression(tmp_expression, 0);
+        tmp_recursiveExpression = tmp_recursiveExpression.substr(1, tmp_closing - 1);
+        before = recursiveExpression(tmp_recursiveExpression, 0);
     }
 
-    std::string notExp = before == true ? "NOT(TRUE)" : "NOT(FALSE)";
+    std::string notExp;
+    if(before == true){
+        notExp = "NOT(TRUE)";
+    }else{
+        notExp = "NOT(FALSE)";
+    }
+    
     return notExp;
 }
 void calculate(std::string input)
 {
-    if (checkValidity(input) && !input.empty())
+    if (checkBracket(input) && !input.empty())
     {
-        if (expression(input, 0) == true)
+        if (recursiveExpression(input, 0) == true)
             std::cout << "TRUE" << std::endl;
         else
             std::cout << "FALSE" << std::endl;
@@ -211,7 +211,6 @@ void calculate(std::string input)
 
 int main()
 {
-    //start
     ProceedInput();
 
     return 0;
