02b50506679daff34506fa1ea078e5d8ef515878
Dmitry <42891046+sidtheslooth20@users.noreply.github.com>
2019-10-15 22:32:50 +0300

    Total huge fix

--- Samakaev/Source/lab2.cpp
+++ Samakaev/Source/lab2.cpp
@@ -2,8 +2,143 @@
 #include <regex>
 #include <string>
 #include <fstream>
+#include <variant>
 #include <map>
 
+
+
+struct Node {
+
+	Node() = default;
+
+	std::variant<char, int> value;
+	std::variant<std::pair<Node*, Node*>, Node*> arguments;
+
+	int calculate();
+};
+
+bool rec_fill_branch(Node*& element, std::string& expression);
+
+int Node::calculate()
+{
+	if (std::holds_alternative<char>(value))
+	{
+		if (std::get<char>(value) == '+')
+			return std::get<std::pair<Node*, Node*>>(arguments).first->calculate() +
+					std::get<std::pair<Node*, Node*>>(arguments).second->calculate();
+		if (std::get<char>(value) == '*')
+			return std::get<std::pair<Node*, Node*>>(arguments).first->calculate() *
+					std::get<std::pair<Node*, Node*>>(arguments).second->calculate();
+		if (std::get<char>(value) == '-')
+		{
+			if (std::holds_alternative<std::pair<Node*, Node*>>(arguments))
+				return std::get<std::pair<Node*, Node*>>(arguments).first->calculate() -
+						std::get<std::pair<Node*, Node*>>(arguments).second->calculate();
+			else
+				return (-1) * std::get<Node*>(arguments)->calculate();
+		}
+		if (std::get<char>(value) == '/')
+			return std::get<std::pair<Node*, Node*>>(arguments).first->calculate() /
+					std::get<std::pair<Node*, Node*>>(arguments).second->calculate();
+	}
+	else
+		return std::get<int>(value);
+	return 0;
+}
+
+bool is_operand(char c) {
+	if (c == '-' || c == '+' || c == '*' || c == '/')
+		return true;
+	else return false;
+}
+
+bool create_node(std::string& expression, int &i, Node* &element) {
+	
+	int bracket_cnt = 0;
+	std::string buff;
+
+	if (expression[i] == '(') {
+		while (true) {
+			buff += expression[i];
+			if (expression[i] == '(') {
+				bracket_cnt++;
+			}
+			else if (expression[i] == ')') {
+				bracket_cnt--;
+			}
+			i++;
+			if (bracket_cnt == 0) {
+				break;
+			}
+		}
+
+		if (!rec_fill_branch(element, buff))
+			return false;
+	}
+	else {
+		while (expression[i] != ' ' && expression[i] != '(' && expression[i] != ')') {
+			if (isdigit(expression[i])) {
+				buff += expression[i];
+				i++;
+			}
+			else
+				return false;
+		}
+		element->value = stoi(buff);
+	}
+
+	return true;
+}
+
+bool rec_fill_branch(Node* &element, std::string& expression) {
+	
+	int i = 1;
+	std::string buff;
+
+	Node* left = new Node;
+	bool f = create_node(expression, i, left);
+
+	if (!f) {
+		return false;
+	}
+
+	while (expression[i] == ' ') {
+		i++;
+	}
+
+	if (is_operand(expression[i])) {
+		if (expression[i] != '-')
+			return false;
+		else {
+			element->value = expression[i];
+			element->arguments = left;
+			return true;
+		}
+	}
+
+	else {
+		Node* right = new Node;
+		f = create_node(expression, i, right);
+
+		if (!f) {
+			return false;
+		}
+
+		while (expression[i] == ' ') {
+			i++;
+		}
+
+		if (is_operand(expression[i]))
+			element->value = expression[i];
+		else
+			return false;
+
+		element->arguments = std::make_pair(left, right);
+		return true;
+	}
+
+	return true;
+}
 
 bool is_brackets_correct(std::string &expression) {
 	
@@ -36,111 +171,22 @@ void fill_map(std::string& values_str, std::map<std::string, int>& arguements_va
 		values_str.erase(match.position(2), match[2].length());
 	}
 
-	std::cout << "Arguements values" << std::endl;
+	//std::cout << "Arguements values" << std::endl;
 
-	for (int i = 0; i < values_str.length(); i++) {
+	for (size_t i = 0; i < values_str.length(); i++) {
 		if (isalpha(values_str[i])) {
 			std::cout << "Incorrect arguement value!" << std::endl;
 			break;
 		}
 	}
 
-	for (auto it = arguements_values_map.begin(); it != arguements_values_map.end(); ++it) {
+	/*for (auto it = arguements_values_map.begin(); it != arguements_values_map.end(); ++it) {
 		std::cout << it->first << " = " << it->second << std::endl;
-	}
-}
-
-void reg_calc(std::string& expression) {
-	//group №         0,1,2     3,4          5   6,7      8         9        10         11
-	std::regex patt("\\((( ){0,}((\\-)?\\d+)( )+)(( ){0,}(\\-?\\d+)( )+){0,}([\\-+*\\/])( ){0,}\\)");
-
-	std::smatch match;
-	int is_like_pattern = 0;
-	int calc_res = 0;
-	bool is_binary = false;
-	int space_index = -1;
-
-	std::cout << "Calculation:" << std::endl;
-	while (is_like_pattern = std::regex_search(expression, match, patt)) {
-		
-		std::string str_to_insert;
-		if (match[10] == '+') {
-			std::cout << expression << std::endl;
-			if (!match[6].matched) {
-				str_to_insert = " " + match[3].str() + " ";
-				expression.replace(match.position(0), match[0].length(), str_to_insert);
-			}
-			else {
-				calc_res = stoi(match[3]) + stoi(match[8]);
-				str_to_insert = " " + std::to_string(calc_res) + " ";
-				expression.erase(match.position(8), match[8].length());
-				expression.replace(match.position(1), match[1].length(), str_to_insert);
-			}
-		}
-		else if (match[10] == '-') {
-			std::cout << expression << std::endl;
-			if (!match[6].matched) {
-				str_to_insert = " " + match[3].str() + " ";
-				expression.replace(match.position(0), match[0].length(), str_to_insert);
-				if (!is_binary) {
-					if (match[4].matched)
-						expression.erase(match.position(4) - 1, 1);
-					else
-						expression.insert(match.position(3), "-");
-				}
-				is_binary = true;
-			}
-			else {
-				calc_res = stoi(match[3]) - stoi(match[8]);
-				str_to_insert = " " + std::to_string(calc_res) + " ";
-				expression.erase(match.position(8), match[8].length());
-				expression.replace(match.position(1), match[1].length(), str_to_insert);
-				is_binary = true;
-			}
-		}
-		else if (match[10] == '*') {
-			std::cout << expression << std::endl;
-			if (!match[6].matched) {
-				str_to_insert = " " + match[3].str() + " ";
-				expression.replace(match.position(0), match[0].length(), str_to_insert);
-			}
-			else {
-				calc_res = stoi(match[3]) * stoi(match[8]);
-				str_to_insert = " " + std::to_string(calc_res) + " ";
-				expression.erase(match.position(8), match[8].length());
-				expression.replace(match.position(1), match[1].length(), str_to_insert);
-			}
-		}
-		else if (match[10] == '/') {
-			std::cout << expression << std::endl;
-			if (match[8].str() == "0")
-				std::cout << "Devide by zero!" << std::endl;
-			else {
-				if (!match[6].matched) {
-					str_to_insert = " " + match[3].str() + " ";
-					expression.replace(match.position(0), match[0].length(), str_to_insert);
-				}
-				else {
-					calc_res = stoi(match[3]) / stoi(match[8]);
-					str_to_insert = " " + std::to_string(calc_res) + " ";
-					expression.erase(match.position(8), match[8].length());
-					expression.replace(match.position(1), match[1].length(), str_to_insert);
-				}
-			}
-		}
-	}
-
-	space_index = expression.find(' ');
-	while (space_index != -1) {
-		expression.erase(space_index, 1);
-		space_index = expression.find(' ');
-	}
-
-	std::cout << "Final reuslt is " << expression << std::endl;
+	}*/
 }
 
 //replace var names with their values
-void substitute(std::string& expression, std::map<std::string, int>& arguements_values_map) {
+bool substitute(std::string& expression, std::map<std::string, int>& arguements_values_map) {
 
 	int pos = 0;
 	std::string buff;
@@ -158,18 +204,20 @@ void substitute(std::string& expression, std::map<std::string, int>& arguements_
 
 	arguements_values_map.clear();
 
-	std::cout << "replaced variables with their values:" << std::endl << expression << std::endl;
+	//std::cout << "replaced variables with their values:" << std::endl << expression << std::endl;
 
-	for (int i = 0; i < expression.length(); i++) {
+	for (size_t i = 0; i < expression.length(); i++) {
 		if (isalpha(expression[i])) {
 			std::cout << "You didn't set all values" << std::endl;
-			return;
+			return false;
 		}
 	}
 
+	return true;
+
 }
 
-void console_input(std::map <std::string, int>& arguements_values_map) {
+void console_input(std::map <std::string, int>& arguements_values_map, Node* head) {
 
 	std::cout << "Please enter an expression" << std::endl;
 	std::string expression;
@@ -184,15 +232,18 @@ void console_input(std::map <std::string, int>& arguements_values_map) {
 	fill_map(values, arguements_values_map);
 
 	if (is_brackets_correct(expression)) {
-		substitute(expression, arguements_values_map);
-		reg_calc(expression);
+		//std::cout << expression << std::endl;
+		if (substitute(expression, arguements_values_map)) {
+			if (rec_fill_branch(head, expression)) {
+				std::cout << head->calculate() << std::endl;;
+			}
+			else
+				std::cout << "Incorrect expression detected" << std::endl;
+		}
 	}
-
-	std::cout << expression << std::endl;
-
 }
 
-void file_input(char* argv, std::map <std::string, int>& arguements_values_map) {
+void file_input(char* argv, std::map <std::string, int>& arguements_values_map, Node* head) {
 
 	std::ifstream file;
 	std::string file_name = argv;
@@ -214,20 +265,27 @@ void file_input(char* argv, std::map <std::string, int>& arguements_values_map) 
 		fill_map(values, arguements_values_map);
 
 		if (is_brackets_correct(expression)) {
-			std::cout << expression << std::endl;
-			substitute(expression, arguements_values_map);
-			reg_calc(expression);
-			std::cout << std::endl;
+			//std::cout << expression << std::endl;
+			if (substitute(expression, arguements_values_map)) {
+				if (rec_fill_branch(head, expression)) {
+					std::cout << head->calculate() << std::endl;;
+				}
+				else
+					std::cout << "Incorrect expression detected" << std::endl;
+			}
 		}
 	}
 }
 
 int main(int argc, char** argv)
 {
+	Node* head = new Node;
+
 	std::map <std::string, int> arguements_values_map;
+
 	if (argc == 1)
-		console_input(arguements_values_map);
+		console_input(arguements_values_map, head);
 	else if (argc == 2)
-		file_input(argv[1], arguements_values_map);
+		file_input(argv[1], arguements_values_map, head);
 	else std::cout << "Please check arguments are correct";
 }
