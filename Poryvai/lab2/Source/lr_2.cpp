e76741fda361bf790a3abec44b32cdf5fd62cc63
Pavel377dq <pad93765@gmail.com>
2019-10-28 23:51:36 +0300

    laba2 fixed

--- Poryvai/lab2/Source/lr_2.cpp
+++ Poryvai/lab2/Source/lr_2.cpp
@@ -1,5 +1,4 @@
-﻿
-#include<iostream>
+﻿#include<iostream>
 #include<cstdlib>
 #include<string>//getline()
 #include<fstream>//ifstream()
@@ -34,9 +33,11 @@ bool isNull(const lisp s);
 void read_lisp(lisp& y);			// основная
 void read_s_expr(char prev, lisp& y);
 void read_seq(lisp& y);
-void read_lisp(lisp& y, std::ifstream &data);
-void read_s_expr(char prev, lisp& y, std::ifstream &data);
-void read_seq(lisp& y, std::ifstream &data);
+void read_lisp(lisp& y, std::ifstream& data);
+void read_s_expr(char prev, lisp& y, std::ifstream& data);
+void read_seq(lisp& y, std::ifstream& data);
+void is_bink1(lisp& y, bool &is_bk);
+void destroy(lisp s);
 
 // функции вывода:
 void write_lisp(const lisp x);		// основная
@@ -51,7 +52,7 @@ lisp head(const lisp s)
 			return s->node.pair.hd;
 		else {
 
-			std::cerr << "Error: Head(atom) \n"; exit(1); 
+			std::cerr << "Error: Head(atom) \n"; exit(1);
 		}
 	else {
 
@@ -65,9 +66,9 @@ lisp tail(const lisp s)
 	if (s != nullptr)
 		if (!isAtom(s))
 			return s->node.pair.tl;
-	else {
+		else {
 			std::cerr << "Error: Tail(atom) \n"; exit(1);
-	}
+		}
 	else {
 		std::cerr << "Error: Tail(nil) \n";
 		exit(1);
@@ -81,6 +82,7 @@ lisp make_atom(int x)
 	s = new s_expr;
 	s->tag = true;
 	s->node.atom = x;
+	//std::cout << x<<"\n";
 	return s;
 
 }
@@ -103,29 +105,45 @@ lisp cons(const lisp h, const lisp t)
 // PreCondition: not isAtom (t)
 {
 	lisp p;
-	if (isAtom(t)) { 
+	if (isAtom(t)) {
 		std::cerr << "Error: Tail(nil) \n"; exit(1);
 	}
 	else {
-			p = new s_expr;
-			p->tag = false;
-			p->node.pair.hd = h;
-			p->node.pair.tl = t;
-			return p;
-		
+		p = new s_expr;
+		p->tag = false;
+		p->node.pair.hd = h;
+		p->node.pair.tl = t;
+		return p;
+
 	}
+}
+void destroy(lisp s)
+{
+	if (s != nullptr) {
+		//std::cout << "del tail: \n";
+		destroy(tail(s));
+
+		delete s->node.pair.hd;
+		//std::cout << "del node: \n";
+		delete s;
+		// s = NULL;
+	};
 }
 void read_lisp(lisp& y)
 {
 	char x;
-	do 
+	do
 		std::cin >> x;
 	while (x == ' ');
+
+
 	read_s_expr(x, y);
+	
 } //end read_lisp
 //...........................
 void read_s_expr(char prev, lisp& y)
 { //prev - ранее прочитанный символ}
+	
 	if (prev == ')') {
 
 		std::cerr << " ! List.Error 1\n"; exit(1);
@@ -139,47 +157,49 @@ void read_s_expr(char prev, lisp& y)
 		y = make_atom(digit);
 	}
 	else read_seq(y);
-} 
+}
 
 void read_seq(lisp& y)
 {
 	char x;
 	lisp p1, p2;
-
+	
 	if (!(std::cin >> x)) {
-
-		std::cerr << " ! List.Error 2 \n"; exit(1); 
+		std::cerr << " ! List.Error 2 \n"; exit(1);
 	}
 	else {
-		while (x == ' ')
+		//std::cout << x;
+		while (x == ' ') {
 			std::cin >> x;
+		}
 		if (x == ')')
 			y = nullptr;
 		else {
-
 			read_s_expr(x, p1);
 			read_seq(p2);
 			y = cons(p1, p2);
 
 		}
 	}
+	
+	
 } //end read_seq
 
-void read_lisp(lisp& y,std::ifstream &data)
+void read_lisp(lisp& y, std::ifstream& data)
 {
 	char x;
-	do 
+	do
 		data >> x;
 	while (x == ' ');
 	//cout << "1";
-	read_s_expr(x, y,data);
-	
+	read_s_expr(x, y, data);
+
 } //end read_lisp
 //...........................
-void read_s_expr(char prev, lisp& y, std::ifstream &data)
+void read_s_expr(char prev, lisp& y, std::ifstream& data)
 { //prev - ранее прочитанный символ}
 	//cout << "2";
-	if (prev == ')') { 
+	if (prev == ')') {
 
 		std::cerr << " ! List.Error 1 \n"; exit(1);
 	}
@@ -190,16 +210,16 @@ void read_s_expr(char prev, lisp& y, std::ifstream &data)
 		y = make_atom(digit);
 
 	}
-	else read_seq(y , data);
+	else read_seq(y, data);
 } //end read_s_expr
 
-void read_seq(lisp& y, std::ifstream &data)
+void read_seq(lisp& y, std::ifstream& data)
 {
 	//cout << "3";
 	char x;
 	lisp p1, p2;
 
-	if (!(data >> x)) { 
+	if (!(data >> x)) {
 		std::cerr << " ! List.Error 2 \n"; exit(1);
 	}
 	else {
@@ -209,8 +229,8 @@ void read_seq(lisp& y, std::ifstream &data)
 			y = nullptr;
 		else {
 			//cout << x << endl;
-			read_s_expr(x, p1,data);
-			read_seq(p2,data);
+			read_s_expr(x, p1, data);
+			read_seq(p2, data);
 			y = cons(p1, p2);
 		}
 	}
@@ -239,7 +259,7 @@ void write_seq(const lisp x)
 	}
 }
 
-void write_lisp(const lisp x,std::ofstream &out)
+void write_lisp(const lisp x, std::ofstream& out)
 {//пустой список выводится как () 
 	if (isNull(x))
 		out << " ()";
@@ -247,16 +267,16 @@ void write_lisp(const lisp x,std::ofstream &out)
 		out << ' ' << x->node.atom;
 	else { //непустой список} 
 		out << " (";
-		write_seq(x , out);
+		write_seq(x, out);
 		out << " )";
 	}
 } // end write_lisp
 //...........................
-void write_seq(const lisp x, std::ofstream &out)
+void write_seq(const lisp x, std::ofstream& out)
 {//выводит последовательность элементов списка без обрамляющих его скобок
 	if (!isNull(x)) {
-		write_lisp(head(x) , out);
-		write_seq(tail(x) , out);
+		write_lisp(head(x), out);
+		write_seq(tail(x), out);
 	}
 }
 
@@ -271,132 +291,172 @@ void sum(lisp p, int* s) {
 		if (p->node.pair.tl != nullptr && !isAtom(p->node.pair.tl->node.pair.hd))
 			sum(p->node.pair.tl, s);
 	}
+
+}
+
+
+void is_bink1(lisp& y,  bool& is_bk) {
+
+	int i = 1;
+	lisp p = y;
+
+	while (p->node.pair.tl != nullptr) {
+
+		i++;
+		p = p->node.pair.tl;
+	}
+	if (i != 2)
+		is_bk = false;
+
+	if (y->node.pair.hd->tag != true)
+		is_bink1(y->node.pair.hd, is_bk);
+
+	if (y->node.pair.tl != nullptr) {
+
+		if (y->node.pair.tl->node.pair.hd->tag != true)
+			is_bink1(y->node.pair.tl->node.pair.hd, is_bk);
+
+	}
+	else
+		is_bk = false;
+
 	
 }
 
-int main(int argc, char* argv[]) {
+
+
+int main(int argc, char *argv[]) {//добавить argc argv
 
 	setlocale(LC_ALL, "Russian");
-	std::cout << "Ввод из файла или из консоли? (f , c)\nОба пункта?(fc)\n";
+	std::cout << "Ввод из файла или из консоли? (f , c)\n";
 	std::string arg;
-	std::getline(std::cin , arg);
+	std::getline(std::cin, arg);
 
 	if (arg == "f") {
-		
-		std::ifstream data(argv[1]);
-		std::ofstream fout("out.txt");//Через linux не пашет
-		
-		if (data) {
-			
-			lisp bin_k;
-			read_lisp(bin_k, data);
 
-			while (bin_k->tag == false) {
+		std::ifstream data(argv[1]);
+		std::ofstream fout("out.txt");
+
+		if (data) {
+
+			lisp bin_k = nullptr;
+			bool is_bk = true;
+			std::string str = "";
+			char ch;
+			
+			
+		
+			while (!data.eof()) {
+				
+				getline(data, str);
+				//след. констр if - ввод и проверка бк
+				if (str != "(" && str != ")" && str != "") {//is_bink1 не подразумевает проверку строки на эти символы
+
+					for (int i = str.length() - 1; i >= 0; i--) 
+						data.putback(str[i]);
+
+					read_lisp(bin_k, data);
+					
+					if (bin_k->node.pair.hd != nullptr && bin_k->node.pair.tl != nullptr) {
+						if (bin_k->node.pair.hd->tag == false && bin_k->node.pair.tl->node.pair.hd->tag == false)//если у нач указателя голова или хвост грузик
+							is_bink1(bin_k, is_bk);
+						else
+							is_bk = false;
+					}
+					else
+						is_bk = false;
+				}
+				else 
+					is_bk = false;
+				
+
 				int total = 0;
+				if (is_bk == true) {
+					
+					write_lisp(bin_k, fout);
+					write_lisp(bin_k);
+					sum(bin_k, &total);
+					std::cout << " sum = " << total << "\n";
+					fout << " sum = ";
+					fout << total;
+					fout << "\n";
+					//read_lisp(bin_k, data);
+				}
+				else {
+					std::cout << "Бк введено неверно\n";
+					fout << "Бк введено неверно\n";
+				}
+				is_bk = true;
+				destroy(bin_k);//освобождение памяти
+				bin_k = nullptr;
+
+			}
+		}
+		else
+			std::cout << "Неверное имя файла\n";
+	}
+	else if (arg == "c") {
+
+		char ch;
+		std::string  str ;
+		lisp bin_k = nullptr;
+		bool is_bk = true;
+
+
+		std::ofstream fout("out.txt");
+		while (!std::cin.get(ch).eof()) {
+
+			std::cin.putback(ch);
+			getline(std::cin, str);
+			//след. констр if - ввод и проверка бк
+			if (str != "(" && str != ")" && str != "") {//тк  is_bink1 не подразумевает проверку строки на эти символы
+
+				for (int i = str.length()-1; i >= 0; i--) {
+					std::cin.putback(str[i]);
+					//std::cout << str[i];
+				}
+
+				read_lisp(bin_k);
+
+
+				if (bin_k->node.pair.hd != nullptr && bin_k->node.pair.tl != nullptr) {
+					if (bin_k->node.pair.hd->tag == false && bin_k->node.pair.tl->node.pair.hd->tag == false)//если у нач указателя голова или хвост грузик
+						is_bink1(bin_k, is_bk);
+					else
+						is_bk = false;
+				}
+				else
+					is_bk = false;
+			}
+			else {
+				is_bk = false;
+			}
+
+			int total = 0;
+
+			if (is_bk == true) {
 
 				write_lisp(bin_k, fout);
 				write_lisp(bin_k);
+
 				sum(bin_k, &total);
+
 				std::cout << " sum = " << total << "\n";
 				fout << " sum = ";
 				fout << total;
 				fout << "\n";
-				read_lisp(bin_k, data);
-
 			}
-		}
-		else
-			std::cout << "Неверное имя файла\n";
-		
-		data.close();
-		fout.close();
-	}
-	else if (arg == "c") {
-
-		lisp bin_k;
-		read_lisp(bin_k);
-		
-		std::ofstream fout("out.txt");
-		while (bin_k->tag == false) {
-
-			int total = 0;
-
-			write_lisp(bin_k, fout);
-			write_lisp(bin_k);
-
-			sum(bin_k, &total);
-
-			std::cout << " sum = " << total<<"\n";
-			fout << " sum = ";
-			fout << total;
-			fout << "\n";
-			read_lisp(bin_k);
-		
-
-		}
-
-		fout.close();
-	}
-	else if (arg == "fc") {
-
-		lisp bin_k;
-
-		std::ifstream data(argv[1]);
-		std::ofstream fout1("out1.txt");
-		if (data) {
-
-			lisp bin_k;
-			read_lisp(bin_k, data);
-
-			std::cout << "File:\n";
-			while (bin_k->tag == false) {
-				int total = 0;
-				
-				write_lisp(bin_k, fout1);
-				write_lisp(bin_k);
-
-				sum(bin_k, &total);
-
-				std::cout << " sum = " << total <<"\n";
-				fout1 << " sum = ";
-				fout1 << total;
-				fout1 << "\n";
-				read_lisp(bin_k, data);
-
-
-			}
-		}
-		else
-			std::cout << "Неверное имя файла\n";
-
-		std::cout << "Consol:\n";
-
-		read_lisp(bin_k);
-
-		std::ofstream fout2("out2.txt");
-
-		
-		while (bin_k->tag == false) {
-
-			int total = 0;
+			else
+				std::cout << "Бк введено неверно\n";
+			is_bk = true;
+			destroy(bin_k);//delete
+			bin_k = nullptr;
 			
-			write_lisp(bin_k, fout2);
-			write_lisp(bin_k);
-
-			sum(bin_k, &total);
-
-			std::cout << " sum = " << total << "\n";
-			fout2 << " sum = ";
-			fout2 << total;
-			fout2 << "\n";
-			read_lisp(bin_k);
 
 
 		}
 		
-		data.close();
-		fout1.close();
-		fout2.close();
 	}
+	
 	return 0;
 }
