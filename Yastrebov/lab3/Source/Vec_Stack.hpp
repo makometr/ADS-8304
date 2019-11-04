763f24a7a1e586abe31cd14d4663e5dafe2eb7ff
Pirozhochek <vanka998@mail.ru>
2019-11-04 21:20:30 +0300

    lr3 done

---
+++ Yastrebov/lab3/Source/Vec_Stack.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include <optional>
+#include <math.h>
+constexpr auto DEFAULT_SIZE = 1<<8;
+
+typedef std::optional<int> nInt;
+
+class Vec_Stack {
+private:
+	int* stack;
+	int index;
+	int size;
+
+public:
+	Vec_Stack(size_t startSize = DEFAULT_SIZE) {
+		stack = new int[startSize];
+		size = startSize;
+		index = -1;
+	}
+
+	void push(int element) {
+		
+		if (index == (size - 1))
+		{
+			int* newStack = new int[size + DEFAULT_SIZE];
+
+			std::copy(&stack[0], &stack[size], &newStack[0]);
+
+			delete[] stack;
+
+			stack = newStack;
+
+			size += DEFAULT_SIZE;
+
+			stack[++index] = element;
+		}
+
+		else {
+			stack[++index] = element;
+		}
+	}
+
+	nInt pop() {
+		if (index == -1)
+			return std::nullopt;
+		else
+			return stack[index--];
+	}
+
+	bool isEmpty() {
+		return (index == -1);
+	}
+
+	nInt top() {
+		if (index != -1)
+			return stack[index];
+		else
+			return std::nullopt;
+	}
+
+	~Vec_Stack() {
+		delete[] stack;
+	}
+};
