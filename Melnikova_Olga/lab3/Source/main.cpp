14b6c38748d9050f9e252e4291a80afc71f02efa
olga-alexandrovnaa <melnikova.oa00@gmail.com>
2019-11-04 20:32:45 +0300

    fixed

--- Melnikova_Olga/lab3/Source/main.cpp
+++ Melnikova_Olga/lab3/Source/main.cpp
@@ -5,9 +5,10 @@
 #include <cstring>
 #include <vector>
 #include <regex.h>
+#include <memory>
 #include "Queue.cpp"
 
-unsigned char IsStringOK(char* str){
+unsigned char IsStringOK(const char* str){
 	unsigned char i = 0;
 	for(i = 0; i<strlen(str); i++){
 		if(!isalpha(str[i])) return 0;
@@ -24,72 +25,69 @@ int GetManID(std::string name, std::vector<std::string>* people, int peopleCount
 	return -1;
 }
 
-void ConsoleReadAndWritePeople(std::vector<std::string>* people, std::vector<int>* parents, std::vector<int>* children, int* peopleCount, int* relationsCount, Queue* queue)
+void ConsoleReadAndWritePeople(std::vector<std::string>* people, std::vector<int>* parents, std::vector<int>* children, int* peopleCount, int* relationsCount, std::unique_ptr<Queue> &queue)
 {
 	// в цикле считываем строки
-	std::cout<<"Введите пары родственников, для завершения\n";
+	std::cout<<"Введите пары родственников, для завершения введите \"-\"\n";
 	std::string str;
 
 	while(str!="-")
 	{
 		getline(std::cin, str, '\n');
 		if(str=="-") continue;
-		char* cstr = new char[str.length()+1];
-		strcpy(cstr, str.c_str());
-		char* name = new char[20];
-		name = strtok (cstr, " ");
 
-		int flagChild = 0;
- 		std::string parentName;
- 		int parentID;
+        int spacePos = str.find(" ");
+		std::string parent = str.substr(0, spacePos);
+		std::string child = str.substr(spacePos + 1, str.length() - spacePos -1);
 
-		// ищем имена в people[]
-		// если имени нет - добавить
+		if (!(IsStringOK(parent.c_str()) && IsStringOK(child.c_str())))
+			exit(1);
 
-		while ((name != NULL) && (IsStringOK(name)))
-		{
-			int flag = 1;
-			for(int i = 0; i < *peopleCount; i++){
-				if(strcmp(name, (*people)[i].c_str()) == 0) { 
-					flag = 0;
-					break;
-					delete cstr;
-					delete name;
-				}
-			}
-			if(flag){
-				people->push_back(name);
-				//people[*peopleCount] = name;
-				(*peopleCount)++;
-			}
+		int parentID;
+		int childID;
 
-			if(flagChild){      // если считали второе и последующие имена в строке, то добавляем отношение
-				parents->push_back(parentID);   //(индекс первого имени в строке - индекс считанного имени)
-				children->push_back(GetManID(name, people, *peopleCount));
-				(*relationsCount)++;
-			}
-			else
-			{
-				parentName = name;
-				parentID = GetManID(parentName, people, *peopleCount);  //функция поиска индекса имени в массиве имен
-			}
-
-			name = new char[20];
-			name = strtok (NULL, " ");
-			flagChild = 1;
+		int flagP = 0;
+		int flagC = 0;
+		for(int i = 0; i < *peopleCount; i++){
+			//if(strcmp(parent.c_str(), (*people)[i].c_str()) == 0)
+			if(parent == (*people)[i]){
+				flagP = 1;
+				parentID = i;
+            }
+			//if(strcmp(child.c_str(), (*people)[i].c_str()) == 0)
+			if(child == (*people)[i]){
+				flagP = 1;
+				childID = i;
+            }
+			if(flagP && flagC)
+				break;
 		}
-		delete cstr;
-		delete name;
-	}
-	std::cout<<"Введите имя, для которого совершать поиск";
+		if(!flagP){
+			people->push_back(parent);
+			parentID = *peopleCount;
+			(*peopleCount)++;
+		}
+		if(!flagC){
+			people->push_back(child);
+			childID = *peopleCount;
+			(*peopleCount)++;
+		}
+
+		parents->push_back(parentID);
+		children->push_back(childID);
+        (*relationsCount)++;
+
+    }
+
+	std::cout<<"Введите имя, для которого совершать поиск:\n";
 	std::string name1;
 	std::cin>>name1;  // если не удалось - выход
 	queue->push(GetManID(name1, people, *peopleCount));
-	if(GetManID(name1, people, *peopleCount)==-1) exit;
+	if(GetManID(name1, people, *peopleCount)==-1) exit(1);
 
 }
 
-void ReadAndWritePeople(std::vector<std::string>* people, std::vector<int>* parents, std::vector<int>* children, int* peopleCount, int* relationsCount, Queue* queue, char* argv1)
+void ReadAndWritePeople(std::vector<std::string>* people, std::vector<int>* parents, std::vector<int>* children, int* peopleCount, int* relationsCount, std::unique_ptr<Queue> &queue, char* argv1)
 {
 	// в цикле считываем строки
 	std::ifstream fin;
@@ -111,66 +109,61 @@ void ReadAndWritePeople(std::vector<std::string>* people, std::vector<int>* pare
        		 exit(1);
 		}
 		if(str=="-") continue;
-		char* cstr = new char[str.length()+1];
-		strcpy(cstr, str.c_str());
-		char* name = new char[20];
-		name = strtok (cstr, " ");
 
-		int flagChild = 0;
- 		std::string parentName;
- 		int parentID;
+		int spacePos = str.find(" ");
+		std::string parent = str.substr(0, spacePos);
+		std::string child = str.substr(spacePos + 1, str.length() - spacePos -1);
 
-		// ищем имена в people[]
-		// если имени нет - добавить
+		if (!(IsStringOK(parent.c_str()) && IsStringOK(child.c_str())))
+			exit(1);
 
-		while ((name != NULL) && (IsStringOK(name)) && (flagChild<2))
-		{
-			int flag = 1;
-			for(int i = 0; i < *peopleCount; i++){
-				if(strcmp(name, (*people)[i].c_str()) == 0) { 
-					flag = 0;	
-					break;
-					delete cstr;
-					delete name;
-				}
-				
-			}
-			if(flag){
-				people->push_back(name);
-				//people[*peopleCount] = name;
-				(*peopleCount)++;
-			}
+		int parentID;
+		int childID;
 
-			if(flagChild){      // если считали второе и последующие имена в строке, то добавляем отношение
-				parents->push_back(parentID);   //(индекс первого имени в строке - индекс считанного имени)
-				children->push_back(GetManID(name, people, *peopleCount));
-				(*relationsCount)++;
-
-			}
-			else
-			{
-				parentName = name;
-				parentID = GetManID(parentName, people, *peopleCount);  //функция поиска индекса имени в массиве имен
-			}
-
-			name = new char[20];
-			name = strtok (NULL, " ");
-			flagChild++;
+		int flagP = 0;
+		int flagC = 0;
+		for(int i = 0; i < *peopleCount; i++){
+			//if(strcmp(parent.c_str(), (*people)[i].c_str()) == 0)
+			if(parent == (*people)[i]){
+				flagP = 1;
+				parentID = i;
+            }
+			//if(strcmp(child.c_str(), (*people)[i].c_str()) == 0)
+			if(child == (*people)[i]){
+				flagP = 1;
+				childID = i;
+            }
+			if(flagP && flagC)
+				break;
 		}
-		delete cstr;
-		delete name;
-	}
+		if(!flagP){
+			people->push_back(parent);
+			parentID = *peopleCount;
+			(*peopleCount)++;
+		}
+		if(!flagC){
+			people->push_back(child);
+			childID = *peopleCount;
+			(*peopleCount)++;
+		}
+
+		parents->push_back(parentID);
+		children->push_back(childID);
+        (*relationsCount)++;
+
+    }
+
 	std::string name1;
 	getline(fin, name1);  // если не удалось - выход
 	queue->push(GetManID(name1, people, *peopleCount));
-	if(GetManID(name1, people, *peopleCount)==-1) exit;
+	if(GetManID(name1, people, *peopleCount)==-1) exit(1);
 
 }
 
 
-int func1(std::vector<std::string> people, std::vector<int> parents, std::vector<int> children, int relationsCount, std::ofstream& fout, Queue* prev_queue, int k){
+int func1(std::vector<std::string> people, std::vector<int> parents, std::vector<int> children, int relationsCount, std::ofstream& fout, std::unique_ptr<Queue> &prev_queue, int k){
+	std::unique_ptr<Queue> queue(new Queue());
 
-	Queue *queue = new Queue;
 	for(int i = 0; i<(prev_queue->size()); i++){
 		for(int j = 0; j<relationsCount; j++){
 			if((prev_queue->get_elem(i))==parents[j]){
@@ -198,7 +191,7 @@ int func1(std::vector<std::string> people, std::vector<int> parents, std::vector
 	}
 	k++;
 	func1(people, parents, children, relationsCount, fout, queue, k);
-	delete queue;
+	return 0;
 }
 
 
@@ -206,7 +199,7 @@ int main(int argc, char *argv[])
 {
     int flag=1;
 	std::vector<std::string> people;
-	Queue *queue = new Queue;
+	std::unique_ptr<Queue> queue(new Queue());
 	std::vector<int> parents;   //индексы имен из people в двух массивах показывают родство
 	std::vector<int> children;
 	int peopleCount = 0;  //количество жителей
@@ -230,9 +223,8 @@ int main(int argc, char *argv[])
         return 0;
     }
 	func1(people, parents, children, relationsCount, fout, queue, 0);
-	
-	delete queue;
-	
+
 	return 0;
 }
+
 
