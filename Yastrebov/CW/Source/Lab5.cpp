f0dc94b9a64a7af06312c9b99ad39b6b1bf0f5c4
Pirozhochek <vanka998@mail.ru>
2019-12-25 08:06:02 +0300

    Yastrebov - CW

---
+++ Yastrebov/CW/Source/Lab5.cpp
@@ -0,0 +1,178 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include "Treap.hpp"
+#include <vector>
+#include <string>
+#include <ctime>
+#include <sstream>
+#include <time.h>
+#include <ctgmath>
+#define TEST_SIZE 1000000
+#define TEST_COUNT 5
+
+
+
+template<typename elem, typename priority>
+void printtree(nodePtr<elem, priority>& head) {
+	if (!head) {
+		std::cout << '#';
+		return;
+	}
+	std::cout << '(';
+	std::cout << "(" << head->key << ";" << head->prior << ")";
+	printtree(head->left);
+	printtree(head->right);
+	std::cout << ')';
+}
+	
+
+
+int main(int argc, char* argv[]) {
+	bool test(false);
+
+	if ((argc == 2) && (*(argv[1]) == 't'))
+		test = true;
+
+	std::streambuf* backup;
+	backup = std::cin.rdbuf();
+
+	std::ifstream ifs;
+
+
+	ifs.open("input.txt");
+
+	std::cin.rdbuf(ifs.rdbuf());
+
+
+	nodePtr<int, int> head = nullptr;
+
+	int value(0);
+
+	srand((unsigned int)time(0));
+
+	if(!test)
+	while (std::cin >> value)
+	{
+		if (!Node<int, int>::search(head, value))
+			Node<int, int>::insert(head, std::make_shared<Node<int, int>>(value, rand() % INT_MAX));
+		else
+			std::cout << "Already there" << std::endl;
+	}
+
+	std::cin.rdbuf(backup);
+	
+
+	if (!test) {
+		std::cout << "tree : " << std::endl;
+
+		printtree(head);
+		std::cout << std::endl;
+	}
+
+
+	for(bool f(1); f && !test; ) {
+		std::cout << "Choose action :\n1 - insert\n2 - erase\n3 - exit" << std::endl;
+
+		int action(0);
+		std::cin >> action;
+
+		switch (action) {
+		case 1:
+			std::cout << "Enter value" << std::endl;
+
+			std::cin >> value;
+
+
+			if (!Node<int, int>::search(head, value))
+				Node<int, int>::insert(head, std::make_shared<Node<int, int>>(value, rand() % INT_MAX));
+			else
+				std::cout << "Already there" << std::endl;
+
+			printtree(head);
+			std::cout << std::endl;
+
+			break;
+				
+		case 2:
+			std::cout << "Enter value" << std::endl;
+
+			std::cin >> value;
+
+			Node<int, int>::erase(head, value);
+
+			printtree(head);
+			std::cout << std::endl;
+
+			break;
+
+		case 3:
+			return 0;
+
+		default:
+			f = !f;
+			break;
+		}
+	}
+
+	
+
+
+	for (int size = 10; size <= TEST_SIZE; size *= 10) {
+		int maxDepth(0), avgDepth(0), maxInsertCalls(0), avgInsertCalls(0), maxEraseCalls(0), avgEraseCalls(0);
+
+		std::cout << "\n_________________________\n";
+		std::cout << "Size of treap : " << size << std::endl;
+
+		for (int j = 0; j < TEST_COUNT; j++) {
+			nodePtr<int, int> testHead = nullptr;
+
+
+			for (int i = 0; i < size; ++i)
+			{
+				if (!Node<int, int>::search(testHead, i))
+					Node<int, int>::insert(testHead, std::make_shared<Node<int, int>>(i, rand() % INT_MAX));
+				else
+					std::cout << "Already there" << std::endl;
+			}
+
+			int tmp = Node<int, int>::depth(testHead);
+			avgDepth += tmp;
+
+			if (tmp > maxDepth)
+				maxDepth = tmp;
+
+			counter = 0;
+
+			Node<int, int>::insert(testHead, std::make_shared<Node<int, int>>(size + 1, rand() % INT_MAX));
+
+
+			avgInsertCalls += counter;
+
+			if (counter > maxInsertCalls)
+				maxInsertCalls = counter;
+
+			counter = 0;
+
+			///
+
+			Node<int, int>::erase(testHead, rand() % size);
+
+
+			avgEraseCalls += counter;
+
+			if (counter > maxEraseCalls)
+				maxEraseCalls = counter;
+
+			counter = 0;
+
+
+		}
+
+		std::cout << "\nTests passed : " << TEST_COUNT << "\nmax depth : " << maxDepth << "\navg depth : " << avgDepth / TEST_COUNT << "\nmax insert calls : " << maxInsertCalls << "\navg insert calls : " << avgInsertCalls / TEST_COUNT
+			<< "\nmax erase calls : " << maxEraseCalls << "\navg erase calls : " << avgEraseCalls / TEST_COUNT;
+	}
+
+	return 0;
+}
+
---
+++ Yastrebov/CW/Source/Treap.hpp
@@ -0,0 +1,143 @@
+#pragma once
+#include <memory>
+#include <algorithm>
+
+static int counter = 0;
+
+template<typename elem, typename priority>
+class Node;
+
+template<typename elem, typename priority>
+using nodePtr = std::shared_ptr<Node<elem, priority>>;
+
+template<typename elem, typename priority>
+class Node {
+public:
+	elem key;
+	priority prior;
+
+	nodePtr<elem, priority> left, right;
+
+	Node() = default;
+
+	//default copy constructor and operator= are intended
+
+	~Node() = default;
+
+	Node(elem key, priority prior) : key(key), prior(prior), left(nullptr), right(nullptr) { }
+
+	static void split(nodePtr<elem, priority>, elem, nodePtr<elem, priority> &, nodePtr<elem, priority> &);
+
+	static void insert(nodePtr<elem, priority> &, nodePtr<elem, priority>);
+
+	static void merge(nodePtr<elem, priority> &, nodePtr<elem, priority>, nodePtr<elem, priority>);
+
+	static bool erase(nodePtr<elem, priority> &, elem);
+
+	static bool search(nodePtr<elem, priority> &, elem);
+	
+	static int depth(nodePtr<elem, priority> &);
+};
+
+
+template<typename elem, typename priority>
+bool Node<elem, priority>::search(nodePtr<elem, priority>& head, elem key)
+{
+	++counter;
+
+	if (!head)
+		return false;
+
+
+	if (head->key == key) 
+		return true;
+
+	return search(head->key < key ? head->right : head->left, key);
+
+
+}
+
+
+
+
+
+template<typename elem, typename priority>
+void Node<elem, priority>::split(nodePtr<elem, priority> head, elem key, nodePtr<elem, priority> & left, nodePtr<elem, priority> & right) {
+	++counter;
+
+	if (!head) {
+		left = nullptr;
+		right = nullptr;
+	}
+
+	else if (key < head->key) {
+		split(head->left, key, left, head->left);
+		right = head;
+	}
+
+	else {
+		split(head->right, key, head->right, right);
+		left = head;
+	}
+}
+
+template<typename elem, typename priority>
+void Node<elem, priority>::insert(nodePtr<elem, priority> &t, nodePtr<elem, priority> it) {
+	++counter;
+
+	if (!t)
+		t = it;
+
+	else if (it->prior > t->prior) {
+		split(t, it->key, it->left, it->right);
+		t = it;
+	}
+
+	else
+		insert(it->key < t->key ? t->left : t->right, it);
+}
+
+template<typename elem, typename priority>
+void Node<elem, priority>::merge(nodePtr<elem, priority> & t, nodePtr<elem, priority> l, nodePtr<elem, priority> r) {
+	++counter;
+
+	if (!l || !r)
+		t = l ? l : r;
+
+	else if (l->prior > r->prior) {
+		merge(l->right, l->right, r); 
+		t = l;
+	}
+
+	else {
+		merge(r->left, l, r->left);
+		t = r;
+	}
+}
+
+template<typename elem, typename priority>
+bool Node<elem, priority>::erase(nodePtr<elem, priority> & t, elem key) {
+	++counter;
+
+	if (!t)
+		return false;
+
+	if (t->key == key) {
+		merge(t, t->left, t->right);
+
+		return true;
+	}
+
+	return erase(key < t->key ? t->left : t->right, key);
+			
+}
+
+
+template<typename elem, typename priority>
+int Node<elem, priority>::depth(nodePtr<elem, priority> &head)
+{
+	if (!head)
+		return 0;
+	
+	return std::max(depth(head->left), depth(head->right)) + 1;
+}
